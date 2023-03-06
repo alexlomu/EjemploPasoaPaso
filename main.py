b15ae133c6684f59824d81893606d3f14f02888b
alexlomu <91721507+alexlomu@users.noreply.github.com>
2023-03-06 17:03:36 +0100

    generadores

---
+++ main.py
@@ -0,0 +1,11 @@
+import asyncio 
+import aiohttp
+from urllib.parse import urlparse
+import sys
+from os import sep
+from sys import stderr
+from bs4 import BeautifulSoup
+from functools import partial
+from generadores import *
+
+
